3a2c7ba85d0ffe5a905b168acbfd1e704ed01c4a
Ivan Volosyuk <Ivan.Volosyuk@gmail.com>
2022-10-08 01:55:41 +1100

    Logger show offline devices.

--- apps/logger/src/main.rs
+++ apps/logger/src/main.rs
@@ -1,13 +1,19 @@
 use anyhow::Result;
+use chrono::DateTime;
 use chrono::{Duration, Local};
 use temperature_protocol::protos::generated::dev::LoggerProto;
+use std::net::IpAddr;
 use std::{collections::HashMap, io::Write};
 use temperature_protocol::fragment_combiner::FragmentCombiner;
 use temperature_protocol::fragment_combiner::MessageHandler;
+use std::sync::mpsc;
+use std::thread;
 
 struct LogPrinter {
     hosts: HashMap<std::net::SocketAddr, u64>,
     last_host: std::net::IpAddr,
+    sender: mpsc::Sender<std::net::IpAddr>,
+    receiver: mpsc::Receiver<i8>,
 }
 
 fn maybe_print_header(last_host: &mut std::net::IpAddr, src: std::net::IpAddr, curr_ts: u64) {
@@ -26,11 +32,57 @@ fn maybe_print_header(last_host: &mut std::net::IpAddr, src: std::net::IpAddr, c
     }
 }
 
+
+
 impl LogPrinter {
     fn new() -> LogPrinter {
+        let (tx, rx) = mpsc::channel();
+        let (tx2, rx2) = mpsc::channel();
+
+        thread::spawn(move || {
+            let mut deadlines : HashMap<IpAddr, DateTime<Local>> = HashMap::new();
+            loop {
+                let time = Local::now();
+                let mut timeout = Duration::minutes(5);
+
+                deadlines.retain(|ip, deadline| {
+
+                    let dt = deadline.signed_duration_since(time);
+                    let expired = dt.le(&Duration::zero());
+                    if expired {
+                        print!(
+                            "===== {0} OFFLINE ({1})\n",
+                            ip.to_string(),
+                            time.format("%a %d %b %H:%M:%S"));
+                    }
+
+                    if dt < timeout { timeout = dt }
+                    !expired
+                });
+
+                if timeout.gt(&Duration::zero()) {
+                    match rx.recv_timeout(timeout.to_std().unwrap()) {
+                        Ok(ip)=> {
+                            let prev = deadlines.insert(ip, Local::now() + Duration::minutes(1));
+                            if prev.is_none() {
+                                print!(
+                                    "===== {0} ONLINE ({1})\n",
+                                    ip.to_string(),
+                                    Local::now().format("%a %d %b %H:%M:%S"));
+                            }
+                            tx2.send(0).unwrap();
+                        },
+                        Err(_) => {}
+                    }
+                } 
+            }
+        });
+
         LogPrinter {
             hosts: HashMap::new(),
             last_host: std::net::IpAddr::V4(std::net::Ipv4Addr::UNSPECIFIED),
+            sender: tx,
+            receiver: rx2,
         }
     }
 }
@@ -47,6 +99,8 @@ impl MessageHandler<LoggerProto> for LogPrinter {
             maybe_print_header(&mut self.last_host, src.ip(), curr_ts);
             println!("------------>8------------");
         }
+        self.sender.send(src.ip())?;
+        self.receiver.recv()?;
 
         for record in &msg.record {
             let ts: u64 = record.ts();
